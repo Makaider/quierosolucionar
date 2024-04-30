54b68fcb56a9d9d03f91669ded2da8f6234d1486
Jaider Ramos <115428225+Makaider@users.noreply.github.com>
2024-04-30 02:39:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const parallax = document.querySelector(".parallax");
+
+    window.addEventListener("scroll", function() {
+        let offset = window.pageYOffset;
+        parallax.style.backgroundPositionY = offset * 0.7 + "px";
+    });
+});
