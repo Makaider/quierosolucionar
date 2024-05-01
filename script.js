5fae15344f5a2be12183613f7bff991e187c567a
Jaider Ramos <115428225+Makaider@users.noreply.github.com>
2024-05-01 01:06:53 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const parallax = document.querySelector(".parallax");
-
-    window.addEventListener("scroll", function() {
-        let offset = window.pageYOffset;
-        parallax.style.backgroundPositionY = offset * 0.7 + "px";
-    });
-});
